ea8da02458d09030fbef003166f0b91ea0aedf49
Daniel Bernardi <taniele.billini@gmail.com>
2010-07-27 10:11:45 +0100

    Updated documentation, renamed plugin

--- README.markdown
+++ README.markdown
@@ -1,6 +1,8 @@
 ## What is Quickon ##
 
-Quickon is a simple Fango plugin that manages multiple configurations on a hostname basis. Currently, database settings and dispatcher's rules are manageable.
+Quickon is a Fango plugin that allows you to have multiple configurations on per-host basis. You can manage database settings and dispatcher rules, but you can also store your own settings in case you need it.
+
+Read the [Wiki](http://wiki.github.com/taniele/Quickon) to get started.
 
 ## How does it work ##
 Simply put your plugin in the `plugins/` directory. Pick the `conf/` directory and place it at the same level of your `application/` or `public/`. The configuration dir is already filled with some examples along with the default file. If there's no file inside the `conf/` directory, Quickon will simply not run.
@@ -9,16 +11,17 @@ Change your app's index.php to include a new instance of Quickon, like this exam
 
 `
 $fango = new Fango();
-$q = new QuickonPlugin($fango);
+$config = QuickonPlugin::init()->getConfig();
+$fango->run($config["rules"]);
 `
 
 If you have a database, remove `FangoDB::connect`. Quickon will take care of this, too.
 
 ## About the configuration files ##
 
-Your configuration files will be in JSON format. You can find a sample configurations in the `default` and `rules` files that you'll find in the `conf/` directory. Those two files specifies the default configuration, such as dispatcher rules.
-Then you have host-specific configuration. Just duplicate `default` and rename it after your app's full hostname (e.g., myapp.example.com) and this file will take priority over the default settings and rules. As you might expect, you can have a host-specific set of dispatcher rules, or simply use the `rules` file to setup global rules. You can also use no rules at all and in this case you'll use the default Fango's rules.
+Your configuration is stored in JSON, and you can find a sample configurations in the `default` and `rules` files that you'll find in the `conf/` directory. Those two files hold the default configuration, such as dispatcher rules.
+Then you have host-specific configuration. Just duplicate `default` and rename it after your app's full hostname (e.g. myapp.example.com) and this file will take priority over the default settings and rules. As you might expect, you can have a host-specific set of dispatcher rules, or simply use the `rules` file to setup global rules. You can also use no rules at all and in this case you'll use the default Fango rules.
 
 ## It's important to know that... ##
 
-Since you're using JSON, notice that literal backslashes (\) and quotes need to be escaped.
+Since you're using JSON, notice that literal backslashes (\\) and quotes need to be escaped.
--- plugins/com.studiomelonpie.quickon.php
+++ plugins/com.studiomelonpie.quickon.php
@@ -1,82 +1,98 @@
 <?
+/**
+ * Quickon - Configuration Plugin for Fango
+ *
+ * @author Daniel Bernardi, Studio Melonpie <dan@studiomelonpie.com>
+ **/
 class QuickonPlugin {
 
-	protected $configProperties = array('database' => array('driver', 'name', 'host', 'user', 'pass'));
-	protected $config;
-	protected $environment = null;
+	/**
+	 * This array specifies which mandatory options must be present for each section. If you need to check for specific sections,
+	 * add them to the main array. The default array checks if 'driver', 'name', 'host', 'user' and 'pass' are all present in
+	 * the section "database" of your configuration file.
+	 *
+	 * @var array
+	 **/
+	protected static $configProperties = array('database' => array('driver', 'name', 'host', 'user', 'pass'));
+	protected static $config;
+	protected static $environment = null;
+	protected static $instance;
 	
 	const CONF_PATH = '../conf/';
-
-	function __construct($fangoInstance, $configName = null) {
-
+	
+	private function __construct($configName = null) {
 		if (($configName === null || !file_exists(QuickonPlugin::CONF_PATH . $configName)) && file_exists(QuickonPlugin::CONF_PATH . $_SERVER["HTTP_HOST"])) {
-			
+
 			$configName = $_SERVER["HTTP_HOST"];
 		}
-		else {
+		elseif(file_exists(QuickonPlugin::CONF_PATH . "default")) {
 			$configName = "default";
 		}
-		
+		else {
+			trigger_error("Quickon: No configuration file found.");
+		}
+
 		$this->config = $this->getConfigFile(QuickonPlugin::CONF_PATH . $configName);
-		$this->parseConfig();
 
 		if (array_key_exists("database", $this->config)) {
+			$this->parseConfig("database");
 			FangoDB::connect($this->config["database"]["driver"] . ":dbname=" . $this->config["database"]["name"] . ";host=" . $this->config["database"]["host"], $this->config["database"]["user"], $this->config["database"]["pass"]);
 		}
 
-		if (array_key_exists("rules", $this->config)) {
-			$fangoInstance->run($this->config["rules"]);
-		}
-		elseif (file_exists(QuickonPlugin::CONF_PATH . 'rules')) {
-			
+		if (file_exists(QuickonPlugin::CONF_PATH . 'rules')) {
+
 			$this->config["rules"] = $this->getConfigFile(QuickonPlugin::CONF_PATH . "rules");
-			$fangoInstance->run($this->config["rules"]);
 		}
-		else {
-			$fangoInstance->run();
+		elseif (!array_key_exists("rules", $this->config)) {
+			$this->config["rules"] = array();
 		}
+		
 	}
 	
-	function getConfigFile($filename) {
+	public static function init($configName = null) {
+		
+		if(!isset(self::$instance)) {
+			$c = __CLASS__;
+			self::$instance = new $c($configName);
+		}		
+
+		return self::$instance;
+	}
+	
+	public function getConfig() {
+		return $this->config;
+	}
+	
+	private function getConfigFile($filename) {
 		return json_decode(file_get_contents($filename), true);
 	}
 	
-	function parseConfig() {
-	
-		foreach ($this->configProperties as $key => $value) {
-			
-			if(!array_key_exists($key, $this->configProperties)) continue;
-			$missingProperties = array_diff_assoc($this->configProperties[$key], array_keys($this->config[$key]));
+		
+	/**
+	 * Parses the the specified section to check if all the required parameters are filled in
+	 *
+	 * @param string $section The configuration section to check.
+	 * @return void
+	 **/
+	private function parseConfig($section) {
 
-			if(!empty($missingProperties)) {
-				throw new Exception("The following properties are missing for section '{$key}': " . implode(',', $missingProperties));
-			}
+		if(!array_key_exists($section, self::$configProperties)) return;
+
+		$missingProperties = array_diff(self::$configProperties[$section], array_keys(self::$config[$section]));
+
+		if(!empty($missingProperties)) {
+			trigger_error("The following properties are missing for section '{$section}': '" . implode("', '", $missingProperties) . "'", E_USER_ERROR);
+			return;
 		}
 		
-		if (isset($this->config["environment"])) {
-			
-			switch (strtolower($this->config["environment"])) {
-				case "development":
-				case "devel":
-				case "dev":
-					$this->environment = "development";
-				break;
-				
-				case "testing":
-				case "staging":
-					$this->environment = "staging";
-				break;
-				
-				case "production":
-				case "live":
-					$this->environment = "live";
-				break;
-			}
+		if (isset(self::$config["environment"])) {
+
+			self::$environment = self::$config;
 		}
 	}
 	
-	function isDevelopment() { return $this->environment == "development"; }
-	function isStaging() { return $this->environment == "staging"; }
-	function isLive() { return $this->environment == "live"; }
+	public function isDevelopment() { return self::$environment == "development"; }
+	public function isStaging() { return self::$environment == "staging"; }
+	public function isLive() { return self::$environment == "live"; }
 	
 }
